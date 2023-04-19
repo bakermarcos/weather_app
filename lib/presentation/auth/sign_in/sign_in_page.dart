33d6383ac376f58aa9650ab3c3cd9b4b5c870cca
Marcos Baker <bakermarcos52@gmail.com>
2023-04-19 15:45:27 -0300

    feat: sign in page

--- lib/presentation/auth/sign_in/sign_in_page.dart
+++ lib/presentation/auth/sign_in/sign_in_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:weather_app/core/config/constants.dart';
+import 'package:weather_app/core/services/local_auth_service.dart';
 
 class SignInPage extends ConsumerStatefulWidget {
   const SignInPage({super.key});
@@ -12,20 +14,24 @@ class SignInPage extends ConsumerStatefulWidget {
 class _SignInPageState extends ConsumerState<SignInPage> {
   @override
   Widget build(BuildContext context) {
+    final authProvider = ref.read(localAuthService);
     return Center(
       child: Column(
         children: [
           Image.asset(imgLogoUncircled),
           const Text('Sign In'),
           Form(
+            key: authProvider.formKey,
             child: Column(
               children: [
                 TextFormField(
+                  controller: authProvider.userTextController,
                   decoration: const InputDecoration(
                     hintText: 'Username',
                   ),
                 ),
                 TextFormField(
+                  controller: authProvider.passwordTextController,
                   decoration: const InputDecoration(
                     hintText: 'Password',
                   ),
@@ -34,11 +40,18 @@ class _SignInPageState extends ConsumerState<SignInPage> {
             ),
           ),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              if (authProvider.formKey.currentState!.validate()) {
+                authProvider.signIn(authProvider.userTextController.text,
+                    authProvider.passwordTextController.text);
+              }
+            },
             child: const Text('Sign In'),
           ),
           InkWell(
-            onTap: () {},
+            onTap: () {
+              context.push('/sign-up');
+            },
             child: const Text('Sign Up'),
           ),
         ],
